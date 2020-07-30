d2238516bcd475ee1fb8678c1b1f0a22277e5375
David Fowler <davidfowl@gmail.com>
2020-07-29 17:33:28 -0700

    No noise

--- 2019/Technical/keynote/BlazorWeather.Server/Startup.cs
+++ 2019/Technical/keynote/BlazorWeather.Server/Startup.cs
@@ -1,15 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using static Weather.Weather;
 using WeatherClientLib;
 using WeatherServiceML;
 
@@ -36,7 +30,7 @@ namespace BlazorWeather
                 options.Address = Configuration.GetServiceUri("weather");
                 Console.WriteLine($"Backend URL is {options.Address}");
             });
-            services.AddScoped<IWeatherClassifier, MLWeatherClassifier>();
+            services.AddScoped<IWeatherClassifier, WeatherClassifier>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
